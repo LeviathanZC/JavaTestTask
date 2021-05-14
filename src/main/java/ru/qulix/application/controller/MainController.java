eb7b4ab9c9a60d4e47e4a74144e9d8dc8720e4c9
LeviathanZC <leviathan.zercomp@gmail.com>
2021-05-14 22:33:36 +0300

    Initial commit

---
+++ src/main/java/ru/qulix/application/controller/MainController.java
@@ -0,0 +1,12 @@
+package ru.qulix.application.controller;
+
+public class MainController {
+
+    /**
+     * stub method for initial commit
+     * @param args - arguments for console input
+     */
+    public static void main(String[] args) {
+    }
+
+}
