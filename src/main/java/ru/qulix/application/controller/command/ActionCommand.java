0b7ff743208678fec019b7991ea4d4cd271c1226
LeviathanZC <leviathan.zercomp@gmail.com>
2021-05-20 00:25:53 +0300

    Desc iface ActionCommand

--- src/main/java/ru/qulix/application/controller/command/ActionCommand.java
+++ src/main/java/ru/qulix/application/controller/command/ActionCommand.java
@@ -1,8 +1,15 @@
 package ru.qulix.application.controller.command;
 
+import ru.qulix.application.controller.router.Router;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @FunctionalInterface
 public interface ActionCommand {
 
-
+    Router execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
 
 }
