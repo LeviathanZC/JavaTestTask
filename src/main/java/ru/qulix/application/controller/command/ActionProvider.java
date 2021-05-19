1d0c154a41d024206a05d048de41e6c95b98829b
LeviathanZC <leviathan.zercomp@gmail.com>
2021-05-20 00:28:21 +0300

    Create ActionProvider

---
+++ src/main/java/ru/qulix/application/controller/command/ActionProvider.java
@@ -0,0 +1,16 @@
+package ru.qulix.application.controller.command;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ActionProvider {
+
+    private static Logger log = LogManager.getLogger(ActionProvider.class;
+
+    private ActionProvider() {}
+
+    public static ActionCommand defineAction(String action) {
+        ActionCommand command;
+        return command;
+    }
+}
