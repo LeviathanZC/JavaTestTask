4ff49159f5fd3454a28ac2afd3934835ad1e147e
LeviathanZC <leviathan.zercomp@gmail.com>
2021-05-20 22:19:17 +0300

    End with processing method

--- src/main/java/ru/qulix/application/controller/RequestController.java
+++ src/main/java/ru/qulix/application/controller/RequestController.java
@@ -1,15 +1,36 @@
 package ru.qulix.application.controller;
 
+import ru.qulix.application.controller.command.ActionCommand;
+import ru.qulix.application.controller.command.ActionProvider;
+import ru.qulix.application.controller.router.Router;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class RequestController extends HttpServlet {
 
 
 
-    private void processing(HttpServletRequest request, HttpServletResponse response) {
-
+    private void processing(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ActionCommand command = ActionProvider.defineAction(request.getParameter(RequestParam.COMMAND));
+        Router router = command.execute(request, response);
+        switch (router.getType()) {
+            case FORWARD: {
+                request.getRequestDispatcher(router.getPage()).forward(request, response);
+                break;
+            }
+            case REDIRECT: {
+                response.sendRedirect(router.getPage());
+                break;
+            }
+            case INCLUDE: {
+                request.getRequestDispatcher(router.getPage()).include(request, response);
+                break;
+            }
+        }
     }
 
 }
