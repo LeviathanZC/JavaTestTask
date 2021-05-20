8c3e2a07465db00a52b3cd7083afaa3917135eac
LeviathanZC <leviathan.zercomp@gmail.com>
2021-05-20 21:59:32 +0300

    Pre-finally define method 'defineAction'. Add new ctr for Router

--- src/main/java/ru/qulix/application/controller/command/ActionProvider.java
+++ src/main/java/ru/qulix/application/controller/command/ActionProvider.java
@@ -2,15 +2,24 @@ package ru.qulix.application.controller.command;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import ru.qulix.application.controller.JspPath;
+import ru.qulix.application.controller.router.Router;
 
 public class ActionProvider {
 
-    private static Logger log = LogManager.getLogger(ActionProvider.class;
+    private static Logger log = LogManager.getLogger(ActionProvider.class);
 
     private ActionProvider() {}
 
     public static ActionCommand defineAction(String action) {
-        ActionCommand command;
+        ActionCommand command = (request, response) -> new Router(JspPath.ERROR_404);
+        try {
+            if (action !=null) {
+                //command parsing
+            }
+        } catch (IllegalArgumentException ex) {
+            log.error(ex);
+        }
         return command;
     }
 }
