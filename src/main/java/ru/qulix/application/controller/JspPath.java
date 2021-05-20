5dee7cadfb7f4d548e69252a47116149bfdee5ec
LeviathanZC <leviathan.zercomp@gmail.com>
2021-05-20 21:39:50 +0300

    Add to JSP error paths

--- src/main/java/ru/qulix/application/controller/JspPath.java
+++ src/main/java/ru/qulix/application/controller/JspPath.java
@@ -5,5 +5,6 @@ public class JspPath {
     //COMMON
 
     //ERROR
-
+    public static final String ERROR_404 = "/WEB-INF/pages/error/error404.jsp";
+    public static final String ERROR_500 = "/WEB-INF/pages/error/error500.jsp";
 }
---
+++ src/main/java/ru/qulix/application/controller/RequestParam.java
@@ -0,0 +1,13 @@
+package ru.qulix.application.controller;
+
+public class RequestParam {
+
+    //COMMON
+    public static final String LOCALE = "locale";
+        public static final String PAGE = "page";
+        public static final String ID = "id";
+        public static final String COMMAND = "command";
+    //PROJECTS
+    //TASKS
+
+}
